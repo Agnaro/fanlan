4902879e88838bffd75f8e875900d803cf1d063f
Eric Root <Root_Eric@bah.com>
2020-02-03 07:13:09 -0500

    add interface placeholders

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -8,19 +8,56 @@ interface Container {
 // not other Setoids
 export interface Setoid extends Container {
     /**
-     * equals :: Setoid a => a ~> a -> Boolean
+     * fantasy-land/equals :: Setoid a => a ~> a -> Boolean
      */
     equals: (b:Setoid) => boolean;
 }
 
 interface Ord extends Setoid {
+    /**
+     * fantasy-land/lte :: Ord a => a ~> a -> Boolean
+     */
     lte: (b:Ord) => boolean;
 }
 
 interface Semigroupoid extends Container {
+    /**
+     * fantasy-land/compose :: Semigroupoid c => c i j ~> c j k -> c i k
+     */
     compose: (b:Semigroupoid) => Semigroupoid
 }
 
 interface Category extends Semigroupoid {
-    
+    /**
+     * fantasy-land/id :: Category c => () -> c a a
+     */
+    id: () => {}
+}
+
+interface Functor {
+    /**
+     * fantasy-land/map :: Functor f => f a ~> (a -> b) -> f b
+     */
+    map: () => {}
+}
+
+interface Apply extends Functor {
+    /**
+     * fantasy-land/ap :: Apply f => f a ~> f (a -> b) -> f b
+     */
+    ap: () => {}
+}
+
+interface Applicative extends Apply {
+    /**
+     * fantasy-land/of :: Applicative f => a -> f a
+     */
+    of: () => {}
+}
+
+interface Chain extends Applicative {
+    /**
+     * fantasy-land/chain :: Chain m => m a ~> (a -> m b) -> m b
+     */
+    chain: () => {}
 }
