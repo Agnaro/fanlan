f6d731349cadf9b48961bd5108b00c5267bef152
Eric Root <e70347@gmail.com>
2020-02-17 12:13:43 -0500

    identity functor tests

--- test/adts/Identity.spec.ts
+++ test/adts/Identity.spec.ts
@@ -1,5 +1,4 @@
 import { identity, Identity } from "../../lib/adts";
-import { expect } from "chai";
 import { assertAdtIsEql } from "../testHelpers";
 
 describe("$Identity", () => {
@@ -13,6 +12,14 @@ describe("$Identity", () => {
       assertAdtIsEql(
         u.map(a => a),
         u
+      );
+    });
+    it("u['fantasy-land/map'](x => f(g(x))) is equivalent to u['fantasy-land/map'](g)['fantasy-land/map'](f) (composition", () => {
+      const f = x => x + 1;
+      const g = x => x * 10;
+      assertAdtIsEql(
+        u.map(x => f(g(x))),
+        u.map(g).map(f)
       );
     });
   });
