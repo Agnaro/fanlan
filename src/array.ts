97b39629420e9b92620db47bc972664494b0831c
Eric Root <Root_Eric@bah.com>
2020-04-01 14:26:18 -0400

    add of to array

--- src/array.ts
+++ src/array.ts
@@ -1,6 +1,10 @@
+// Apply
 // apply :: forall a b. f (a -> b) -> f a -> f b
 // ap :: Array (a -> b) -> Array a -> Array b
-function ap(fnArr,arr){
+function ap<A,B>(
+  fnArr: Array<(a0:A)=>B>, 
+  arr: Array<A>
+): Array<B> {
     const out = [];
     for(let f=0; f < fnArr.length; f++){
       for(let d=0; d < arr.length; d++){
@@ -11,4 +15,7 @@ function ap(fnArr,arr){
   }
   
   // type Apply <= Applicative
-  // of/pure :: 
+  // of/pure :: forall a. Applicative f => a -> f a
+  function of<T>(val:T): Array<T> {
+    return [val]
+  }
